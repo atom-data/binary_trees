d765651ce7d7c2eac1ea541c177cc6f31b0832d7
atom-data <alatatomondi@gmail.com>
2022-11-17 07:45:01 -0800

    insert right-child node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert a node as the right-child of another node
+ * @parent: pointer to node to insert the left-child
+ * @value: value for node
+ * Return: pointer to new right node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node, *rChild;
+
+	if (!parent)
+		return (NULL);
+
+	node = binary_tree_node(parent, value);
+
+	rChild = parent->right;
+	if (rChild)
+	{
+		rChild->parent = node;
+		node->right = rChild;
+		parent->right = node;
+	} else
+	{
+		parent->right = node;
+	}
+	return (node);
+}
